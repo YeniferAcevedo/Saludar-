d230e6cdf0d82288eb0f81f88f44156b9ea253ee
YeniferAcevedo <yeniferacevedo@hotmail.com>
2020-07-27 23:46:12 -0500

    Se crea programa para generar un saludo con un nombre ingresado

---
+++ Saludar!/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Saludar_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsaludar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(344, 252);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(162, 25);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese nombre: ";
+            // 
+            // btnsaludar
+            // 
+            this.btnsaludar.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsaludar.Location = new System.Drawing.Point(314, 310);
+            this.btnsaludar.Name = "btnsaludar";
+            this.btnsaludar.Size = new System.Drawing.Size(89, 43);
+            this.btnsaludar.TabIndex = 3;
+            this.btnsaludar.Text = "Saludar";
+            this.btnsaludar.UseVisualStyleBackColor = true;
+            this.btnsaludar.Click += new System.EventHandler(this.btnsaludar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Saludar_.Properties.Resources.welcome_754x321;
+            this.pictureBox1.Location = new System.Drawing.Point(229, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 171);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnsaludar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnombre);
+            this.Name = "Form1";
+            this.Text = "Saludar!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsaludar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Saludar!/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saludar_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsaludar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola! " + txtnombre.Text, "Mensaje de Bienvenda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
